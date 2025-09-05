ec028574724e3321f28fdaacaa85a2c42639b2d8
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-09-05 17:14:28 +0300

    fix: correct LEFT JOIN query for properties and reviews to include ORDER BY clause

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,11 +4,11 @@ SELECT b.booking_id, b.property_id, b.user_id, b.start_date, b.end_date, b.total
 FROM Booking b
 INNER JOIN "User" u ON b.user_id = u.user_id;
 
--- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
 SELECT p.property_id, p.host_id, p.name, p.description, p.pricepernight, p.created_at, p.updated_at,
 	   r.review_id, r.rating, r.comment, r.user_id, r.created_at AS review_created_at
 FROM Property p
-LEFT JOIN Review r ON p.property_id = r.property_id;
+LEFT JOIN Review r ON p.property_id = r.property_id
+ORDER BY p.name ASC, r.rating DESC NULLS LAST;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
 SELECT u.user_id, u.first_name, u.last_name, u.email, u.role, u.created_at,
