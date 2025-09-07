970600980e576d6467f907e513c7039402b24d06
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-09-07 23:19:54 +0300

    refactor: enhance property ranking query to include ROW_NUMBER() alongside RANK()

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,9 +4,16 @@ FROM Booking b
 INNER JOIN "User" u ON b.user_id = u.user_id
 GROUP BY b.user_id, u.first_name, u.last_name;
 
--- 2. Rank properties based on the total number of bookings they have received
-SELECT p.property_id, p.name, COUNT(b.booking_id) AS booking_count,
-       RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank
-FROM Property p
-LEFT JOIN Booking b ON p.property_id = b.property_id
-GROUP BY p.property_id, p.name;
+-- 2. Rank properties based on total number of bookings using RANK() and ROW_NUMBER()
+SELECT *
+FROM (
+    SELECT 
+        p.property_id, 
+        p.name, 
+        COUNT(b.booking_id) AS booking_count,
+        RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_rank,
+        ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS property_row_number
+    FROM Property p
+    LEFT JOIN Booking b ON p.property_id = b.property_id
+    GROUP BY p.property_id, p.name
+)
